657aced56f23b68c4d7097e928a4ca1a32fd288f
sssy913 <99971751+sssy913@users.noreply.github.com>
2022-03-10 19:22:46 +0900

    [2022년 0310] 프로젝트 생성 및 톰캣 서버 설정 -jdk 1.8 -tomcat server9.0 -news 폴더 생성

---
+++ demo20220310/src/main/java/com/example/demo/Demo20220310Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo20220310Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo20220310Application.class, args);
+	}
+
+}
